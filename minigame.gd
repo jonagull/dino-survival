5070813edbfa6770ea34607f87086ce157436cac
Boien <59290906+jonagull@users.noreply.github.com>
2025-08-22 10:57:10 +0200

    init player2

--- minigame.gd
+++ minigame.gd
@@ -17,7 +17,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		current_player = player
 		# area.get_parent().set_brush(self)
 
-
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if (area.name == "PlayerArea"):
 		label.visible = false
@@ -25,9 +24,7 @@ func _on_area_2d_area_exited(area: Area2D) -> void:
 		player.clear_brush()
 		current_player = null
 
-
 func chop():
-	print("chop")
 	hp -= 1
 	if hp <= 0:
 		current_player.add_brush(1)
---
+++ player_2.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+@onready var anim_sprite = $AnimatedSprite2D
+@export var speed := 100
+
+var last_direction := "down"
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	pass
+		
+	var input_vector = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	)
+	
+	#current_brush.chop()
+
+	if input_vector.length() > 0:
+		velocity = input_vector.normalized() * speed
+		move_and_slide()
+
+		# Determine direction
+		if abs(input_vector.x) > abs(input_vector.y):
+			if input_vector.x > 0:
+				last_direction = "right"
+			else:
+				last_direction = "left"
+		else:
+			if input_vector.y > 0:
+				last_direction = "down"
+			else:
+				last_direction = "up"
+
+		anim_sprite.animation = "walk_" + last_direction
+		anim_sprite.play()
+	else:
+		velocity = Vector2.ZERO
+		anim_sprite.animation = "idle_" + last_direction
+		anim_sprite.play()
+	
+func _unhandled_input(event):
+	pass
+
+func _process(delta):
+	pass
